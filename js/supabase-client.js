5d8ce4c2c380eddc94dd790583be693602b64bca
Atika76 <139039295+Atika76@users.noreply.github.com>
2026-03-16 19:12:14 +0100

    Delete js directory

--- js/supabase-client.js
+++
@@ -1,8 +0,0 @@
-window.sb = window.supabase.createClient(APP_CONFIG.supabaseUrl, APP_CONFIG.supabaseKey, {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: true,
-    flowType: 'pkce'
-  }
-});
